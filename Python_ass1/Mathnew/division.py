fe398ba2ceb88c85011f515c6578f865d0215439
abhinav kumar <abhinavkool19@gmail.com>
2020-08-15 20:12:24 +0530

    new

---
+++ Python_ass1/Mathnew/division.py
@@ -0,0 +1,5 @@
+def div(a,b):
+    if(b==0):
+        return("Can't divided by 0")
+    else:
+        return(a/b)    
---
+++ Python_ass1/solution.py
@@ -0,0 +1,11 @@
+import Mathnew.addition as ad
+import Mathnew.substraction as su
+import Mathnew.multipication as mu
+import Mathnew.division as di
+import Mathnew.sqroot as sq
+
+print(sq.sqrt(7))
+print(ad.add(2,10))
+print(su.sub(4,6))
+print(mu.mult(78,95))
+print(di.div(12,0))
